467168f14434a21c0dfd19e601656fe09e644eca
Mark Joseph Livado <livadomc@gmail.com>
2026-01-24 00:40:10 +0800

    chore: add vercel analytics

--- app/sentinel-web/src/app/page.tsx
+++ app/sentinel-web/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@/components/common';
 import { HeroSection, FeatureSection } from '@/app/(public)/landing';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Header />
       <HeroSection />
       <FeatureSection />
