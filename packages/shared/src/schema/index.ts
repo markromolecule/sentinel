5f3fc7b916d27c7338c9627bc74246d09bdbcbef
Mark Joseph Livado <livadomc@gmail.com>
2026-02-09 09:22:25 +0800

    chore: update schema folder

--- packages/shared/src/schema/index.ts
+++ packages/shared/src/schema/index.ts
@@ -1,8 +1,8 @@
 
-export * from "./LoginSchema";
-export * from "./RegisterSchema";
-export * from "./UserSchema";
-export * from "./SubjectSchema";
-export * from "./AnnouncementSchema";
-export * from "./AssignmentSchema";
-export * from "./ExamConfigSchema";
+export * from "./auth/LoginSchema";
+export * from "./auth/RegisterSchema";
+export * from "./users/UserSchema";
+export * from "./subjects/SubjectSchema";
+export * from "./announcements/AnnouncementSchema";
+export * from "./assignments/AssignmentSchema";
+export * from "./exams/ExamConfigSchema";
